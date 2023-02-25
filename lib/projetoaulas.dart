74f63633ab1e13330ecbcc181d1d044264b79dd3
Vinicius Ferrari <ferrarivinicius4@gmail.com>
2023-02-24 22:40:11 -0300

    Primeiro Commit

---
+++ lib/projetoaulas.dart
@@ -0,0 +1,6 @@
+//Pasta Lib é onde colocamos o codigo//
+
+
+int calculate() {
+  return 6 * 7;
+}
---
+++ test/projetoaulas_test.dart
@@ -0,0 +1,8 @@
+import 'package:projetoaulas/projetoaulas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
