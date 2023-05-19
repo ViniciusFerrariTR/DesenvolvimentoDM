da9b1e1b9be88ab8fedc9a30c150c018093b5566
Vini Ferrari <vipferrari@hotmail.com>
2023-05-18 22:37:54 -0300

    Organizada pastas do projeto e criado telas

---
+++ Segundo Trimestre/projetoVirtusShop/lib/telas/TelaApresentacaoUm.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TelaApresentacaoTres extends StatelessWidget {
+  const TelaApresentacaoTres({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ApresentaçãoUm',
+            style: TextStyle(fontWeight: FontWeight.bold)),
+      ),
+      body: Container(),
+    );
+  }
+}
