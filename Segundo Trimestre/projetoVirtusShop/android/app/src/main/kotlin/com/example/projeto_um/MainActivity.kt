a002b73ef9fe5d6fa348a090ff695fd47c37bb73
Vini Ferrari <vipferrari@hotmail.com>
2023-05-31 20:19:35 -0300

    Retirado pasta projetoVirtusShop

--- Segundo Trimestre/projetoVirtusShop/android/app/src/main/kotlin/com/example/projeto_um/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projeto_um
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Segundo Trimestre/projetoVirtusShop/lib/rotas.dart
+++
@@ -1,9 +0,0 @@
-class Rotas {
-  static const home = "/";
-  static const login = "login";
-  static const cadastro = "cadastro";
-  static const apresentacaoUm = "apresentacaoUm";
-  static const apresentacaoDois = "apresentacaoDois";
-  static const apresentacaoTres = "apresentacaoTres";
-  static const esqueciSenha = "esqueciSenha"; 
-}
