e627ae8b4f12c03b3330445fa0066f28acdc2eae
Vini Ferrari <vipferrari@hotmail.com>
2023-05-31 20:47:55 -0300

    Criado pasta da aula do dia 09/05

---
+++ Segundo Trimestre/desenvolvimentoAulas/aula_0905/android/app/src/main/kotlin/com/example/aula_0905/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aula_0905
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Segundo Trimestre/desenvolvimentoAulas/aula_0905/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:aula_0905/app.dart';
+
+void main() {
+  runApp(const App());
+}
